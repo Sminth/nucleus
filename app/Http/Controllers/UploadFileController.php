77c0931077e25c1d3b7fd0762c82e70f2285140a
sminth <emmanuelmalan225@gmail.com>
2020-09-26 06:11:55 +0000

    resolution de bug

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -12,18 +12,18 @@ class UploadFileController extends Controller
         return view('upload');
      }
      public function showUploadFile(Request $req) {
-      //  dd($req);
+        
         $req->validate([
             'file' => 'required|mimes:csv,txt,xls,pdf,doc,docx,png,jpg,jpeg,mp4|max:15000'
             ]);
             $ext_image=array('png','jpg','jpeg');
             $ext_doc=array('docx','doc','csv','txt','xls','pdf');
             $ext_video=array('mp4');
-            
+            $fileModel = new fichiers;
+    
             if($req->file()) {
                 $fileName = $req->file->getClientOriginalName();
                 $extension=$req->file->getClientOriginalExtension();
-                
                 if (in_array($extension, $ext_image)) {
                     $filePath = $req->file('file')->move('fichiers/images', $fileName);
                     $type="image";$lien="images/$fileName";
@@ -36,25 +36,20 @@ class UploadFileController extends Controller
                     $filePath = $req->file('file')->move('fichiers/videos', $fileName);
                     $type="video";$lien="videos/$fileName";
                 }
-                else{
-                    $filePath = $req->file('file')->move('fichiers/documents', $fileName);
-                    $type="document";$lien="documents/$fileName";
-                }
-                
                 User::create(["name"=>$req->name,"titre_fichier_ajouter"=>$req->title]);
-                fichiers::insert(["title"=>$req->title,"description"=>$req->description,"keywords"=>$req->keywords,"lien"=>$lien,"type"=>$type]);
-                //dd($req->title,$req->description,$req->keywords,$lien,$type);
-                
-                /*  
-                $fileModel->name = time().'_'.$req->file->getClientOriginalName();
-                $fileModel->file_path = '/storage/' . $filePath;
-                $fileModel->save();
-    */  
+
+               // fichiers::create(["title"=>$req->title,"description"=>$req->description,"keywords"=>$req->keywords,"lien"=>$lien,"type"=>$type]);
     
-                return redirect()->back()->with('success','Votre fichier a bien été enregistré.')->with('file', $fileName);
-            }
-            else{
-                dd($req->file->getClientOriginalName());
+                $fileModel->title = $req->name;
+                $fileModel->description = $req->description;
+                $fileModel->keywords = $req->keywords;
+                $fileModel->lien = $lien;
+                $fileModel->type = $type;
+                $fileModel->save();
+    
+                return redirect()->back()
+                ->with('success','Votre fichier a bien été enregistré.')
+                ->with('file', $fileName);
             }
         }
   }
