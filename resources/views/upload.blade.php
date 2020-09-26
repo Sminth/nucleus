b7ba8864945dd8c7a3cf6e1bee36e4dc28d3ac08
sminth <emmanuelmalan225@gmail.com>
2020-09-26 12:18:01 +0000

    affichage du nom de fichier apres l'upload

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -62,4 +62,12 @@
             </div>
         </form>
     </div>
+    <script>
+        $('#chooseFile').on('change',function(){
+            //get the file name
+            var fileName = $(this).val();
+            //replace the "Choose a file" label
+            $(this).next('.custom-file-label').html(fileName);
+        })
+    </script>
 @endsection
